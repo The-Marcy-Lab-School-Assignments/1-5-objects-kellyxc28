c2eb0cc585e820752af6108758e33c764816d74b
kellyxc28 <kellyxiongc@gmail.com>
2024-04-24 14:37:02 -0400

    Added Comments For Review

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,10 +1,10 @@
 /** FEEDBACK: Great job! You have all test cases passing! */
 const coolGreeting = (person) => {
-    if (person.isCool === true) {
-      return `What is UP ${person.name.toUpperCase()}? How you been doin'?`;
+    if (person.isCool === true) { // if person is cool 
+      return `What is UP ${person.name.toUpperCase()}? How you been doin'?`;  // use uppercase 
     }
     else {
-      return `Greetings ${person.name}, how have you been lately?`;
+      return `Greetings ${person.name}, how have you been lately?`; // otherwise, type as regular cased 
     }
 };
 
@@ -15,7 +15,7 @@ const haveBirthday = (person) => {
 
 const becomeSecretAgent = (person, spyHandle) => {
     delete person.name; 
-    person.spyHandle = spyHandle;
+    person.spyHandle = spyHandle; // adding value (stored in spyHandle variable), to property called spyHandle 
 };
 
 const carMaker = (name, maker, year) => {
@@ -24,40 +24,42 @@ const carMaker = (name, maker, year) => {
       maker: maker,
       year: year,
     }
-    car.needsOilChange = false;
+    car.needsOilChange = false; // default value, if no other value is given 
     return car;
 };
 
 const weAreNotFriends = (person) => {
-    const lastFriend = person.friends.pop();
+    const lastFriend = person.friends.pop();  // remove element at the end 
     return lastFriend;
 };
 
 const listHobbies = (person) => {
-    for (let i = 0; i < person.hobbies.length; i++) {
+    for (let i = 0; i < person.hobbies.length; i++) { // go through person object 
+      // print out person's name and what hobbies they like 
       console.log(`${person.name} likes ${person.hobbies[i]}.`);
     }
 };
 
 const getNextOpponent = (team) => {
+    // if the matches property of teams object is empty 
     if (team.matches.length === 0) {
       return null;
     }
-    return team.matches[0].teamName;
+    return team.matches[0].teamName;  // return the teamName of the first opponent in matches 
 };
 
 const listAllKeys = (object) => {
-    return Object.keys(object);
+    return Object.keys(object);   // keys of an object 
 };
 
 const listAllValues = (object) => {
-    return Object.values(object);
+    return Object.values(object);   // values of an object 
 };
 
 const convertToMatrix = (arr) => {
     const matrixArr = [];
 
-    if (arr.length === 0) {
+    if (arr.length === 0) {   // if array is empty 
       return [];
     }
 
